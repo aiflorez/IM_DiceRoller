bb82cdbb879810243b1af6faafbc1a7d09b2ca2f
aiflorez <60967650+aiflorez@users.noreply.github.com>
2022-03-01 12:00:01 +0100

    Comportamiento adicional RollDice

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.diceroller
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity()
 {
@@ -20,9 +20,29 @@ class MainActivity : AppCompatActivity()
 
     private fun rollDice() {
         val dice = Dice(6)
+        val nlucky = 3
         val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        val resultNumber: TextView = findViewById(R.id.txNumber)
+        resultNumber.text = diceRoll.toString()
+
+        val resultLucky: TextView = findViewById(R.id.txLucky)
+        when(diceRoll)
+        {
+            1 -> resultLucky.text ="So sorry! You rolled a 1. Try again!"
+            2 -> resultLucky.text ="So sorry! You rolled a 2. Try again!"
+            nlucky -> resultLucky.text ="You are a winner!!!"
+            4 -> resultLucky.text ="So sorry! You rolled a 4. Try again!"
+            5 -> resultLucky.text ="So sorry! You rolled a 5. Try again!"
+            6 -> resultLucky.text ="So sorry! You rolled a 6. Try again!"
+        }
+
+        if(diceRoll==nlucky)
+        {
+            resultLucky.setTextColor(Color.GREEN)
+        }else
+        {
+            resultLucky.setTextColor(Color.RED)
+        }
     }
 }
 
